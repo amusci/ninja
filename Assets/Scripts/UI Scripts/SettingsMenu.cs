523cc8513c2dd567bb97756e2272ca2feca215c7
Antonio Musciano <71357098+frky1@users.noreply.github.com>
2024-05-07 22:51:43 -0400

    setting up settings lol

---
+++ Assets/Scripts/UI Scripts/SettingsMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public void settingsMenu()
+    {
+
+        SceneManager.LoadScene("Start Menu");
+
+    }
+
+}
