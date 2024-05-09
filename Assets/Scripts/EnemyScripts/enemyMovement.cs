4ac9588a58d68f25615706a5730ca53f7e50df3c
Antonio Musciano <71357098+frky1@users.noreply.github.com>
2024-05-08 21:37:36 -0400

    enemy patrol

---
+++ Assets/Scripts/EnemyScripts/enemyMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public Transform[] patrolPoints; // array of patrol points
+    public float moveSpeed; // control enemy speed
+    public int patrolDestination; // control which point we want to move to
+
+    void Update()
+    /* Called once per frame */
+    {
+        if (patrolDestination == 0) // if we want to go to the 0 point (regardless of where we are)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime); // move towards the point
+
+            if (Vector2.Distance(transform.position, patrolPoints[0].position) < .2f) // if we are .2f close to the point we want to reach
+            {
+
+                patrolDestination = 1; // next point
+
+            }
+        }
+        if (patrolDestination == 1) // if we want to go to the 1 point (regardless of where we are)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
+            //move towards the point
+
+            if (Vector2.Distance(transform.position, patrolPoints[1].position) < .2f) // if we are .2f close
+            {
+
+                patrolDestination = 0; // set to next point
+
+            }
+
+        }
+
+    }
+}
+
