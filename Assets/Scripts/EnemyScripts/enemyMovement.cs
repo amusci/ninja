7cbf3cbee2f12435fa556b9071a2334460d90e73
Antonio Musciano <71357098+frky1@users.noreply.github.com>
2024-05-09 22:41:54 -0400

    follow player, still need to work on how to handle when player runs away and other physics

--- Assets/Scripts/EnemyScripts/enemyMovement.cs
+++ Assets/Scripts/EnemyScripts/enemyMovement.cs
@@ -1,15 +1,75 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyMovement : MonoBehaviour
 {
-
-    public Transform[] patrolPoints; // array of patrol points
     public float moveSpeed; // control enemy speed
+    public Transform[] patrolPoints; // array of patrol points
     public int patrolDestination; // control which point we want to move to
+
+    private Transform player; // referencing the player's transform
+
+    public float detectionRange = .2f; // range of detection
+
+    private bool followingPlayer; // false when not following, true when following
+
+    public Text followingPlayerText; // Reference to the UI Text element
+
+    void Start()
+    {
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
 
     void Update()
     /* Called once per frame */
     {
+
+        if (!followingPlayer) // if the enemy hasnt found a player 
+        {
+
+            Patrol(); // patrol
+            checkForPlayer(); // check for player while patroling
+
+        }
+        else // player detected
+        {
+
+            FollowPlayer(); //follow player 
+
+        }
+
+
+    }
+
+
+    void FollowPlayer()
+    /* Moves enemy to the player */
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, player.position, (moveSpeed + 2) * Time.deltaTime); // move towards player 
+
+    }
+    void checkForPlayer()
+    /* Check to see if player is in the detection range of enemy */
+    {
+
+        if (Vector2.Distance(transform.position, player.position) <= detectionRange) // if player in detection range
+        {
+
+            followingPlayer = !followingPlayer; // flip bool
+
+
+        }
+
+    }
+
+
+    void Patrol()
+    /* function that handles enemy patrol */
+    {
+
         if (patrolDestination == 0) // if we want to go to the 0 point (regardless of where we are)
         {
 
@@ -41,4 +101,3 @@ public class EnemyMovement : MonoBehaviour
 
     }
 }
-
